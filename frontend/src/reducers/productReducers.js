72956746c499e47129da984fd101d0770177010d
Arif Rasim <arifrasim1@gmail.com>
2021-05-07 18:56:53 +0300

    modified:   backend/server.js modified:   frontend/src/actions/productActions.js 	modified:   frontend/src/constants/productConstants.js 	modified:   frontend/src/reducers/productReducers.js 	modified:   frontend/src/screens/ProductScreen.js 	modified:   frontend/src/store.js

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,10 +1,16 @@
 const {
-    PRODUCT_LIST_REQUEST,
-    PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+  PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
-} = require('../constants/productConstants');
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
+} = require("../constants/productConstants");
 
-export const productListReducer = (state={ loading:true,products:[]}, action) => {
+export const productListReducer = (
+  state = { loading: true, products: [] },
+  action
+) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
       return { loading: true };
@@ -16,3 +22,19 @@ export const productListReducer = (state={ loading:true,products:[]}, action) =>
       return state;
   }
 };
+
+export const productDetailsReducer = (
+    state = { product: {}, loading: true },
+    action
+  ) => {
+    switch (action.type) {
+      case PRODUCT_DETAILS_REQUEST:
+        return { loading: true };
+      case PRODUCT_DETAILS_SUCCESS:
+        return { loading: false, product: action.payload };
+      case PRODUCT_DETAILS_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
+    }
+  };
