6b774c96bf8f7b350273196e86978d2b137248af
Arif Rasim <arifrasim1@gmail.com>
2021-05-10 18:47:11 +0300

    Implemented Place Order action

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import dotenv from "dotenv";
+import OrderRouter from "./routers/orderRouter.js";
 dotenv.config();
 
 const app = express();
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/eyn", {
 app.use("/api/users", userRouter);
 
 app.use("/api/products", productRouter);
+app.use('/api/orders',OrderRouter)
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
